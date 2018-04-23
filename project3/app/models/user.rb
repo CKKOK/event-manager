094711dfb7c751d6fa532d180510742cb3ba3031
CK <kok.chee.kean@outlook.com>
2018-04-23 13:44:01 +0800

    Fixed user name entry on creating user

--- project3/app/models/user.rb
+++ project3/app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :name, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
-  attr_accessor :name
   
   has_and_belongs_to_many :users_events
 end
