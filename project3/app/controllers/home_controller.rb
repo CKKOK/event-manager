389ad800061a2d19517154c2fae63d6abe95c8af
CK <kok.chee.kean@outlook.com>
2018-04-27 14:56:58 +0800

    fixed pluralization issue in home controller

--- project3/app/controllers/home_controller.rb
+++ project3/app/controllers/home_controller.rb
@@ -3,7 +3,7 @@
 class HomeController < ApplicationController
   def index
     if !current_user.nil?
-      redirect_to users_path(current_user)
+      redirect_to user_path(current_user)
     end
   end
 end
