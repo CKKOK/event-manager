b9919779077473eefd7550e3cfe691aa6d2b7ad6
CK <kok.chee.kean@outlook.com>
2018-05-01 22:12:04 +0800

    output rails s to server.log

--- project3/startserver.sh
+++ project3/startserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 redis-server &
-rails s -b 'ssl://0.0.0.0:3000?key=localhost.key&cert=localhost.crt' &
+rails s -b 'ssl://0.0.0.0:3000?key=localhost.key&cert=localhost.crt' > server.log &
